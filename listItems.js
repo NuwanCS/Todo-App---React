08339cb7275ac189fa3ac32436dd415c8e1374d0
Nuwan Senevirathne <64064155+NuwanCS@users.noreply.github.com>
2020-05-19 19:31:56 +0530

    Upload-01

---
+++ listItems.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./listItems.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function ListItems(props) {
+  const items = props.items;
+  const ListItems = items.map((item) => {
+    return (
+      <div className="list" key={item.key}>
+        <p>
+          {item.text}
+          <span>
+            <FontAwesomeIcon
+              className="faicons"
+              icon="trash"
+              onClick={() => props.deleteItem(item.key)}
+            />
+          </span>
+        </p>
+      </div>
+    );
+  });
+  return <div>{ListItems}</div>;
+}
+
+export default ListItems;
